6f805be1e4686c428993941b572bb3c6e8e0c608
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-10-28 17:52:18 +0600

    fix handleClick function

--- src/Containers/App.tsx
+++ src/Containers/App.tsx
@@ -1,11 +1,33 @@
-import reactLogo from '../assets/react.svg';
+import React, {useState} from 'react';
+import GameBoard from '../components/GameBoard/GameBoard.tsx';
+import {CellData} from '../types';
 import './App.css';
 
-function App() {
+const createItems = () => {
+  const items: CellData[] = [];
+  for (let i = 0; i < 36; i++) {
+    items.push({hasItem: false, clicked: false});
+  }
+  const randomIndex = Math.floor(Math.random() * items.length);
+  items[randomIndex].hasItem = true;
+  return items;
+};
+
+const App = () => {
+  const [items, setItems] = useState<CellData[]>(createItems());
+
+  const handleCellClick = (index: number) => {
+    const updatedItems: CellData[] = [...items];
+    updatedItems[index].clicked = true;
+    setItems(updatedItems);
+  };
+
 
   return (
-
+    <div>
+      <GameBoard items={items} handleClick={(index) => handleCellClick(index)}></GameBoard>
+    </div>
   );
-}
+};
 
 export default App;
